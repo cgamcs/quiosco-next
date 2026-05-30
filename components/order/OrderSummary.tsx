e9efe40d43f866af85e64219ca0271d15704f17f
cgamcs <cesar.garciamcs@proton.me>
2026-05-30 14:47:48 -0600

    mostrando los articulos en la orden

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useStore } from "@/src/store"
+import ProductDetails from "./ProductDetails"
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order)
@@ -11,7 +12,12 @@ export default function OrderSummary() {
 
       {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> : (
         <div className="mt-5">
-          <p>Si hay algo</p>
+          {order.map(item => (
+            <ProductDetails
+              key={item.id}
+              item={item}
+            />
+          ))}
         </div>
       )}
     </aside>
