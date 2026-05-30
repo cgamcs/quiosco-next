761191804c74e938891961b5b4f6695be62d7cfc
cgamcs <cesar.garciamcs@proton.me>
2026-05-30 12:59:05 -0600

    agregando lib para data fetching

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,4 +1,13 @@
-export default function OrderSidebar() {
+import { prisma } from "@/src/lib/prisma"
+
+async function getCategories() {
+  return await prisma.category.findMany()
+}
+
+export default async function OrderSidebar() {
+  const cateogries = await getCategories()
+  console.log(cateogries)
+
   return (
     <aside className="md:w-72 md:h-screen bg-white">
       OrderSidebar
