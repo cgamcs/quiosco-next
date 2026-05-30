46f96fd2efde65ca367de7d12d6ba738607d4346
cgamcs <cesar.garciamcs@proton.me>
2026-05-30 13:13:27 -0600

    creando el navemenu de la barra de navegacion

---
+++ components/ui/CategoryIcon.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import { Category } from "@/src/generated/prisma/client"
+
+type CategoryIconProps = {
+  category: Category
+}
+
+export default function CategoryIcon({category} : CategoryIconProps) {
+  return (
+    <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+      <div className="relative size-16">
+        <Image
+          src={`/icon_${category.slug}.svg`}
+          alt={`Imagen de la Categoría: ${category.name}`}
+          fill
+        />
+      </div>
+
+      <p className="text-lg font-bold">{category.name}</p>
+    </div>
+  )
+}
