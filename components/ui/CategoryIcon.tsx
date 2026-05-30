2a4d848f1749e104fbdef157fd97cb55d8bc6dd8
cgamcs <cesar.garciamcs@proton.me>
2026-05-30 13:57:03 -0600

    agregando resaltado en la navegacion

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 import { Category } from "@/src/generated/prisma/client"
 
 type CategoryIconProps = {
@@ -7,8 +10,10 @@ type CategoryIconProps = {
 }
 
 export default function CategoryIcon({category} : CategoryIconProps) {
+  const params = useParams<{category: string}>()
+
   return (
-    <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+    <div className={`${category.slug === params.category ? 'bg-amber-500' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
       <div className="relative size-16">
         <Image
           src={`/icon_${category.slug}.svg`}
