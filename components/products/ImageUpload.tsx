52df913e21b9c6eb19e0b4e9f6b57b4b797db14a
César García <cesar.garciamcs@proton.me>
2026-06-01 20:31:31 +0000

    obteniendo url de la imagen

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,9 +1,13 @@
 "use client"
 
 import { CldUploadWidget } from "next-cloudinary"
+import Image from "next/image"
+import { useState } from "react"
 import { TbPhotoPlus } from "react-icons/tb"
 
 export default function ImageUpload() {
+  const [imageUrl, setImageUrl] = useState('')
+
   return (
     <CldUploadWidget
       uploadPreset="ml_default"
@@ -11,7 +15,11 @@ export default function ImageUpload() {
         maxFiles: 1
       }}
       onSuccess={(result, {widget}) => {
-        console.log(result)
+        if (result.event === "success") {
+          widget.close()
+          // @ts-ignore
+          setImageUrl(result.info.secure_url)
+        }
       }}
     >
       {({open}) => (
@@ -24,8 +32,27 @@ export default function ImageUpload() {
             >
               <TbPhotoPlus size={50} />
               <p className="text-lg font-semibold">Agregar Imagen</p>
+
+              {imageUrl && (
+                <div
+                  className="absolute inset-0 w-full h-full"
+                >
+                  <Image
+                    fill
+                    style={{objectFit: "contain"}}
+                    src={imageUrl}
+                    alt="Imagen de Producto"
+                  />
+                </div>
+              )}
             </div>
           </div>
+
+          <input
+            type="hidden"
+            name="image"
+            value={imageUrl}
+          />
         </>
       )}
     </CldUploadWidget>
