a88c01e987b40924eb87307b1f3abd6d1b660149
César García <cesar.garciamcs@proton.me>
2026-06-01 17:45:02 +0000

    composicion para usar server components dentro de client components

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,13 +1,17 @@
-import ProductForm from "./ProductForm";
+"use client"
 
+export default function AddProductForm({children}: {children: React.ReactNode}) {
+  const handleSubmit = async (formData: FormData) => {
+    console.log('desde handleSubmit')
+  }
 
-export default function AddProductForm() {
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
       <form
         className="space-y-5"
+        action={handleSubmit}
       >
-        <ProductForm />
+        {children}
         
         <input
           type="submit"
