faeaf7f9abbb5d137f4ae02270c2973b35c02cad
César García <cesar.garciamcs@proton.me>
2026-06-01 17:00:27 +0000

    validando la busqueda con zod

--- components/products/ProductSearchForm.tsx
+++ components/products/ProductSearchForm.tsx
@@ -1,7 +1,29 @@
+"use client"
+
+import { SearchSchema } from "@/src/schema"
+import { redirect } from "next/navigation"
+import { toast } from "react-toastify"
 
 export default function ProductSearchForm() {
+  const handleSearchForm = (formData: FormData) => {
+    const data = {
+      search: formData.get('search')
+    }
+    const result = SearchSchema.safeParse(data)
+
+    if (!result.success) {
+      result.error.issues.forEach(issue => {
+        toast.error(issue.message)
+      })
+      return
+    }
+
+
+    redirect(`/admin/products/search?search?search=${result.data.search}`)
+  }
+
   return (
-    <form className="flex items-center">
+    <form action={handleSearchForm} className="flex items-center">
       <input
         type="text"
         name="search"
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -15,3 +15,7 @@ export const OrderSchame = z.object({
 export const OrderIdSchema = z.object({
   orderId: z.string().transform((value) => parseInt(value)).refine(value => value > 0, {message: 'Hay errores en el ID de la orden'})
 })
+
+export const SearchSchema = z.object({
+  search: z.string().trim().min(1, {message: 'La búsqueda no puede ir vacía'})
+})
