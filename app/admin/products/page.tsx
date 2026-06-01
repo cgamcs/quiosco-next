496a61f45e27df5042df64082322bb30d34593b0
César García <cesar.garciamcs@proton.me>
2026-06-01 16:34:15 +0000

    agregando proteccion en la url para la paginacion

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,6 +2,7 @@ import Pagination from "@/components/products/Pagination"
 import ProductTable from "@/components/products/ProductTable"
 import Heading from "@/components/ui/Heading"
 import { prisma } from "@/src/lib/prisma"
+import { redirect } from "next/navigation"
 
 async function productCount() {
   return await prisma.product.count()
@@ -27,11 +28,14 @@ export default async function ProductsPage({searchParams} : {searchParams: { pag
   const page = +searchParams.page || 1
   const pageSize = 10
 
+  if (page < 0) redirect('/admin/products')
+
   const prodcutsData = getProducts(page, pageSize)
   const totalProductsData = productCount()
   const [ prodcuts, totalProducts ] = await Promise.all([prodcutsData, totalProductsData])
   const totalPages = Math.ceil(totalProducts / pageSize)
 
+  if (page > totalPages || page < 0) redirect('/admin/products')
   return (
     <>
       <Heading>
