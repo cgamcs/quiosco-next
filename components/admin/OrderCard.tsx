3e70dcf726de1c43a9957202a5eacaf475142320
cgamcs <cesar.garciamcs@proton.me>
2026-05-31 18:28:29 -0600

    mostrando el contenido de la orden

--- components/admin/OrderCard.tsx
+++ components/admin/OrderCard.tsx
@@ -14,6 +14,19 @@ export default function OrderCard({ order }: OrderCardProps) {
       <p className="text-2xl font-medium text-gray-900">Cliente: {order.name}</p>
       <p className="text-lg font-medium text-gray-900">Productos Ordenados:</p>
       <dl className="mt-6 space-y-4">
+        {order.orderProducts.map(product => (
+          <div
+            key={product.id}
+            className="flex items-center gap-2 border-t border-gray-200 pt-4"
+          >
+            <dt className="flex items-center text-sm text-gray-600">
+              <span className="font-black">({product.quantity} {''})</span>
+            </dt>
+
+            <dd className="text-sm font-medium text-gray-900">{product.product.name}</dd>
+          </div>
+        ))}
+
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <dt className="text-base font-medium text-gray-900">
             Total a Pagar:
